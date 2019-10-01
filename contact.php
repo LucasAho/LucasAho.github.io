b44552a33d0ff19aca203a6faef61199d705a3f7
Lucas Aho <aholucas11@gmail.com>
2019-09-30 20:50:13 -0600

    Uploading current progress

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Page</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+        <header>
+            <h1>Lucas Aho</h1>
+
+            <nav id="navHead">
+                <a href="index.html">About</a>
+                <a href="portfolio.html">Portfolio</a>
+                <a href="">Contact</a>                
+            </nav>
+        </header>
+
+        <section>
+            <h2>Contact</h2>
+
+            <form method="post" action="contact.php">
+                <textarea name="message"></textarea>
+                <input type="submit">
+            </form>
+           
+
+            <div>
+                Long and funny statements that make me seem cool and helpful yo yo. 
+            </div>
+        </section>
+
+
+
+    </body>
+
+
+
+</html>
